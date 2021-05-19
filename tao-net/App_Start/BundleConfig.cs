34560ffa87f49319cda9e3f0e127d29fe76aa1bc
Ali Raza Akhtar <alix0731@stud.kea.dk>
2021-05-20 00:40:26 +0200

    ny forside og om os

--- tao-net/App_Start/BundleConfig.cs
+++ tao-net/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace tao_net
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                        "~/Content/style.css"));
         }
     }
 }
